f9dc36252afd1772fa6f98e01b5bd6938e826f3e
Pavani Donepudi <pavani.donepudi@gmail.com>
2014-07-13 05:24:29 -0700

    PLot 3 code and graph

---
+++ plot3.R
@@ -0,0 +1,13 @@
+# Read the Data into R and convert the Date column to be of Date type. 
+MasterData <- read.csv(file="household_power_consumption.txt", header=TRUE, sep=";")
+MasterData <- transform(MasterData, Date = as.Date(Date,"%d/%m/%Y"))
+workData <- subset(MasterData, Date == "2007-02-01" | Date == "2007-02-02")
+workData <- transform(workData,Global_active_power = as.numeric(as.character(Global_active_power)))
+workData <- transform(workData, Time = format(strptime(Time,format = "%X"),format = "%H:%M:%S"))
+workData <- transform(workData, Time = as.POSIXct(paste(Date, Time), format="%Y-%m-%d %H:%M:%S"))
+workData <- transform(workData,Sub_metering_1 = as.numeric(as.character(Sub_metering_1)))
+workData <- transform(workData,Sub_metering_2 = as.numeric(as.character(Sub_metering_2)))
+with(workData, plot(Time, Sub_metering_1,type = "l",col = "black",xlab = "", ylab = "Energy sub metering"))
+with(workData, lines(Time, Sub_metering_2,type = "l",col = "red"))
+with(workData, lines(Time, Sub_metering_3,type = "l",col = "blue"))
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=c(1,1,1), col = c("black","red","blue"))
